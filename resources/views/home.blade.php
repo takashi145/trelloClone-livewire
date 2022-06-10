ea5f13250d6354960314fee2d7d5f0195a5de25e
takashi145 <t.shuhei0203@gmail.com>
2022-06-10 21:50:11 +0900

    一覧画面作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,27 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   @livewireStyles
 </head>
 <body>
-  
 
+  <header class="position-fixed w-100 d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <h3>
+      Livewire Trello Clone
+    </h3>
+  </header>
+
+  <main>
+    <div class="pt-5 container-fluid">
+      <div class="pt-5">
+        <livewire:card-list />
+      </div>
+    </div>
+  </main>
+  
+  
   @livewireScripts
+  <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
