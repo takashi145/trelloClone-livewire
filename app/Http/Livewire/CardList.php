ade1143895a51d6e13cd700ac548c38692aa1bc0
takashi145 <t.shuhei0203@gmail.com>
2022-06-10 22:36:44 +0900

    カード作成機能

--- app/Http/Livewire/CardList.php
+++ app/Http/Livewire/CardList.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Card;
 use Livewire\Component;
 
 class CardList extends Component
 {
+    public $cardName;
+    public $taskName;
+
+    protected $rules = [
+        'cardName' => 'required|max:30'
+    ];
+
+    protected $messages = [
+        'cardName.required' => '必須項目です。',
+        'cardName.max' => '30文字以内で入力してください。',
+    ];
+
+    public function createCard()
+    {
+        $this->validate();
+
+        Card::create([
+            'name' => $this->cardName,
+        ]);
+        $this->cardName = "";
+    }
+
+    public function createTask()
+    {
+        dd($this->taskName);
+    }
+
     public function render()
     {
-        return view('livewire.card-list');
+        return view('livewire.card-list',
+            ['cards' => Card::all()]
+        );
     }
 }
--- resources/views/livewire/card-list.blade.php
+++ resources/views/livewire/card-list.blade.php
@@ -1,23 +1,26 @@
 <div class="m-3">
     <div class="row flex-nowrap">
-        @for($i = 0; $i < 3; $i++)
+        @foreach($cards as $card)
         <div class="col-5 col-md-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title border-bottom border-1">Title</h5>
+                    <h5 class="card-title border-bottom border-1">{{ $card->name }}</h5>
                     @for($j = 0; $j < rand(1, 10); $j++)
                     <p class="card-text">task</p>
                     @endfor
-                    <form action="" method="get">
-                        <input type="text" class="form-control" placeholder="タスクを追加">
+                    <form wire:submit.prevent="createTask">
+                        <input wire:model="taskName" type="text" class="form-control" placeholder="タスクを追加">
                         <input type="submit" class="invisible">
                     </form>
                 </div>
             </div>
         </div>
-        @endfor
-        <form class="col-5 col-md-3">
-            <input type="text" class="form-control" placeholder="新しいカードを追加">
+        @endforeach
+        <form wire:submit.prevent="createCard" class="col-5 col-md-3">
+            <input wire:model="cardName" type="text" class="form-control" placeholder="新しいカードを追加">
+            @error('cardName')
+                <p class="text-danger m-1">{{ $message }}</p>
+            @enderror
             <input type="submit" class="invisible">
         </form>
     </div>
